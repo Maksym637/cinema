7a39e89a7676f66550289ceb29f3553693e97374
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-10-01 23:51:00 +0300

    first models of DB

--- django_cinema_project/app_film/models.py
+++ django_cinema_project/app_film/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    languange = models.CharField(max_length=50)
+    year = models.DateTimeField()
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return f"id         : {self.pk}\n" \
+               f"name       : {self.name}\n" \
+               f"genre      : {self.genre}\n" \
+               f"language   : {self.languange}\n" \
+               f"year       : {self.year}\n" \
+               f"rate       : {self.rate}\n"
--- django_cinema_project/app_hall/models.py
+++ django_cinema_project/app_hall/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Hall(models.Model):
+
+    number = models.IntegerField()
+    max_people = models.IntegerField()
+
+    def __str__(self):
+        return f"id         : {self.pk}\n" \
+               f"number     : {self.number}\n" \
+               f"max_people : {self.max_people}\n"
