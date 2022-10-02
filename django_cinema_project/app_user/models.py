32c945cc5600d4d13582d7cfe9aed16d664bd855
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-10-02 20:10:05 +0300

    New updates with models

--- django_cinema_project/app_user/models.py
+++ django_cinema_project/app_user/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
 
 
-class User(AbstractBaseUser):
+class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
@@ -18,3 +17,13 @@ class User(AbstractBaseUser):
     def create(cls, username, first_name, last_name, email, password, phone, is_admin):
         return cls(username=username, first_name=first_name, last_name=last_name, 
                    email=email, password=password, phone=phone, is_admin=is_admin)
+    
+    def __str__(self):
+        return f"id         : {self.pk}\n" \
+               f"username   : {self.username}\n" \
+               f"first_name : {self.first_name}\n" \
+               f"last_name  : {self.last_name}\n" \
+               f"email      : {self.email}\n" \
+               f"password   : {self.password}\n" \
+               f"phone      : {self.phone}\n" \
+               f"is_admin   : {self.is_admin}\n"    
--- django_cinema_project/fill_models.py
+++ django_cinema_project/fill_models.py
@@ -8,7 +8,7 @@ from app_user.models import User
 
 
 def main():
-    user1 = User.objects.create('u1', 'user1', 'user1', 'user1@gmail.com', '1111', '1111', False)
+    user1 = User.create('u1', 'user1', 'user1', 'user1@gmail.com', '1111', '1111', False)
     # user2 = User(2, 'u2', 'user2', 'user2', 'user2@gmail.com', '2222', '2222', False)
     # user3 = User(3, 'u3', 'user3', 'user3', 'user3@gmail.com', '3333', '3333', False)
 
@@ -16,6 +16,7 @@ def main():
     # user2.save()
     # user3.save()
 
+    print(User.objects.all()[0])
     # for i in range(len(User.objects.all())):
     #     print(User.objects.all()[i])
 
