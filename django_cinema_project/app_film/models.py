af9c4949c30a0ab4b8a80b2d6da262374f7f7575
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-10-18 15:15:06 +0300

    Set fileds : language and genre as ENUM in FilmM

--- django_cinema_project/app_film/models.py
+++ django_cinema_project/app_film/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+from django_enumfield import enum
+
+
+class Languages(enum.Enum):
+    ENG, SPA, FRE, UKR = 1, 2, 3, 4
+
+    __labels__ = {
+        ENG: 'English',
+        SPA: 'Spanish',
+        FRE: 'French',
+        UKR: 'Ukrainian'
+    }
+
+
+class Genres(enum.Enum):
+    ACTION, COMEDY, DRAMA, FANTASY, HORROR = 1, 2, 3, 4, 5
+
+    __labels__ = {
+        ACTION: 'Action',
+        COMEDY: 'Comedy',
+        DRAMA: 'Drama',
+        FANTASY: 'Fantasy',
+        HORROR: 'Horror'
+    }
 
 
 class Film(models.Model):
@@ -7,8 +31,8 @@ class Film(models.Model):
         ordering = ['year']
 
     name = models.CharField(max_length=100)
-    genre = models.CharField(max_length=50)
-    language = models.CharField(max_length=50)
+    genre = enum.EnumField(Genres, default=Genres.ACTION)
+    language = enum.EnumField(Languages, default=Languages.ENG)
     image = models.ImageField(null=True)
     year = models.DateField()
     rate = models.IntegerField()
@@ -22,6 +46,6 @@ class Film(models.Model):
                f"name       : {self.name}\n" \
                f"genre      : {self.genre}\n" \
                f"language   : {self.language}\n" \
-               f"image url  : {self.image.url}\n" \
+               f"image url  : {self.image}\n" \
                f"year       : {self.year}\n" \
                f"rate       : {self.rate}"
